590bd9b54b9d0f750b10af128f9d8309e8395ac3
Charanjit Singh <ckhabra007@gmail.com>
2015-11-10 07:35:43 +0530

    Callback based base for inspirational console.

--- clcn-app/promise.js
+++ clcn-app/promise.js
@@ -1,1 +1,14 @@
 /* global superagent, Rx */
+let apiUrl;
+
+apiUrl = 'http://localhost:8000/inspiration';
+
+superagent
+    .get(apiUrl)
+    .end((err, res) => {
+        let inspiration;
+
+        inspiration = JSON.parse(res.text).joke;
+
+        console.log(inspiration);
+    });

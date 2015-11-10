16cb9d6e301233659963752e16b1dd167a10b419
Charanjit Singh <ckhabra007@gmail.com>
2015-11-11 01:27:26 +0530

    Convert callbacks to Promise and Observable

--- clcn-app/observable.js
+++ clcn-app/observable.js
@@ -1,14 +1,37 @@
 /* global superagent, Rx */
-let apiUrl;
+let Observable,
+    apiUrl,
+    reactiveInspiration;
 
 apiUrl = 'http://localhost:8000/inspiration';
+Observable = Rx.Observable;
 
-superagent
-    .get(apiUrl)
-    .end((err, res) => {
-        let inspiration;
+reactiveInspiration = Observable.create((observer) => {
+    superagent
+        .get(apiUrl)
+        .end((err, res) => {
+            if (err) {
+                return observer.onError(err);
+            }
 
-        inspiration = JSON.parse(res.text).joke;
+            let inspiration;
 
-        console.log(inspiration);
-    });
+            inspiration = JSON.parse(res.text).joke;
+
+            observer.onNext(inspiration);
+            observer.onCompleted();
+        });
+});
+
+reactiveInspiration
+    .subscribe(
+        (inspiration) => {
+            console.log('Get inspired: ', inspiration);
+        },
+        (err) => {
+            console.log('Error while getting inspired', err);
+        },
+        () => {
+            console.log('Done getting inspired!');
+        }
+    );
--- clcn-app/promise.js
+++ clcn-app/promise.js
@@ -1,14 +1,27 @@
 /* global superagent, Rx */
-let apiUrl;
+let apiUrl,
+    promiscuousInspiration;
 
 apiUrl = 'http://localhost:8000/inspiration';
 
-superagent
-    .get(apiUrl)
-    .end((err, res) => {
-        let inspiration;
+promiscuousInspiration = new Promise((resolve, reject) => {
+    superagent
+        .get(apiUrl)
+        .end((err, res) => {
+            if (err) {
+                return reject(err);
+            }
 
-        inspiration = JSON.parse(res.text).joke;
+            let inspiration;
 
-        console.log(inspiration);
+            inspiration = JSON.parse(res.text).joke;
+            return resolve(inspiration);
+        });
+});
+
+promiscuousInspiration
+    .then((inspiration) => {
+        console.log('Get Inspired: ', inspiration);
+    }, (err) => {
+        console.error('Error occurred while getting inspiration.', err);
     });

ae14047e8a71a58b145836644c5aca564b542bf5
Charanjit Singh <ckhabra007@gmail.com>
2015-11-11 12:14:30 +0530

    Inspirational HTML
    One small curried function and we go from inpirational console to inspirational HTML

--- clcn-app/observable.js
+++ clcn-app/observable.js
@@ -1,17 +1,27 @@
 /* global superagent, Rx */
 let Observable,
     apiUrl,
+    inspireHTML,
     reactiveInspiration;
 
 apiUrl = 'http://localhost:8000/inspiration';
 Observable = Rx.Observable;
 
+inspireHTML = (parentId) => (inspiration) => {
+    let parentNode,
+        inspirationalNode;
+
+    parentNode = document.getElementById(parentId);
+    inspirationalNode = document.createElement('p');
+    inspirationalNode.innerHTML = inspiration;
+
+    parentNode.insertBefore(inspirationalNode, parentNode.firstChild);
+};
+
 reactiveInspiration = Observable.create((observer) => {
-    console.log("Sending request.");
     superagent
         .get(apiUrl)
         .end((err, res) => {
-            console.log('Got response');
             if (err) {
                 return observer.onError(err);
             }
@@ -23,23 +33,19 @@ reactiveInspiration = Observable.create((observer) => {
             observer.onNext(inspiration);
             observer.onCompleted();
 
-            // Dispose function which is guaranteed to get called when Observable stops.
-            // All resource-freeing work shall be done in here.
             return () => {
                 console.log('Release the Kraken!');
             };
         });
 });
 
-// reactiveInspiration
-//     .subscribe(
-//         (inspiration) => {
-//             console.log('Get inspired: ', inspiration);
-//         },
-//         (err) => {
-//             console.log('Error while getting inspired', err);
-//         },
-//         () => {
-//             console.log('Done getting inspired!');
-//         }
-//     );
+reactiveInspiration
+    .subscribe(
+        inspireHTML('inspiration'),
+        (err) => {
+            console.log('Error while getting inspired', err);
+        },
+        () => {
+            console.log('Done getting inspired!');
+        }
+    );
--- clcn-app/promise.js
+++ clcn-app/promise.js
@@ -1,15 +1,25 @@
 /* global superagent, Rx */
 let apiUrl,
+    inspireHTML,
     promiscuousInspiration;
 
 apiUrl = 'http://localhost:8000/inspiration';
 
+inspireHTML = (parentId) => (inspiration) => {
+    let parentNode,
+        inspirationalNode;
+
+    parentNode = document.getElementById(parentId);
+    inspirationalNode = document.createElement('p');
+    inspirationalNode.innerHTML = inspiration;
+
+    parentNode.insertBefore(inspirationalNode, parentNode.firstChild);
+};
+
 promiscuousInspiration = new Promise((resolve, reject) => {
-    console.log('Sending request.');
     superagent
         .get(apiUrl)
         .end((err, res) => {
-            console.log('Got response');
             if (err) {
                 return reject(err);
             }
@@ -21,9 +31,9 @@ promiscuousInspiration = new Promise((resolve, reject) => {
         });
 });
 
-// promiscuousInspiration
-//     .then((inspiration) => {
-//         console.log('Get Inspired: ', inspiration);
-//     }, (err) => {
-//         console.error('Error occurred while getting inspiration.', err);
-//     });
+promiscuousInspiration
+    .then(
+        inspireHTML('inspiration'),
+        (err) => {
+            console.error('Error occurred while getting inspiration.', err);
+        });

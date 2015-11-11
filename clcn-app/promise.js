c4149fd2dce4676558c2a50edf3fbdd4e09b03ba
Charanjit Singh <ckhabra007@gmail.com>
2015-11-11 12:27:25 +0530

    Make promise add 10 inspirations instead of just 1

--- clcn-app/promise.js
+++ clcn-app/promise.js
@@ -1,7 +1,9 @@
 /* global superagent, Rx */
 let apiUrl,
+    addNInspirations,
+    addPromiscuousInspiration,
     inspireHTML,
-    promiscuousInspiration;
+    getPromiscuousInspiration;
 
 apiUrl = 'http://localhost:8000/inspiration';
 
@@ -16,7 +18,7 @@ inspireHTML = (parentId) => (inspiration) => {
     parentNode.insertBefore(inspirationalNode, parentNode.firstChild);
 };
 
-promiscuousInspiration = new Promise((resolve, reject) => {
+getPromiscuousInspiration = () => new Promise((resolve, reject) => {
     superagent
         .get(apiUrl)
         .end((err, res) => {
@@ -31,9 +33,34 @@ promiscuousInspiration = new Promise((resolve, reject) => {
         });
 });
 
-promiscuousInspiration
-    .then(
-        inspireHTML('inspiration'),
-        (err) => {
-            console.error('Error occurred while getting inspiration.', err);
-        });
+addPromiscuousInspiration = (promiscuousInspiration) => {
+    promiscuousInspiration
+        .then(
+            inspireHTML('inspiration'),
+            (err) => {
+                console.error('Error occurred while getting inspiration.', err);
+            });
+};
+
+addNInspirations = (n) => {
+    let count,
+        interval,
+        maxCount;
+
+    maxCount = n;
+    count = 1;
+
+    interval = setInterval(() => {
+        addPromiscuousInspiration(
+            getPromiscuousInspiration()
+        );
+
+        if (count < maxCount) {
+            count++;
+        } else {
+            clearInterval(interval);
+        }
+    }, 1000);
+};
+
+addNInspirations(10);
